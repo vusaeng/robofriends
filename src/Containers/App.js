f40c045e618ffc697c72e60431e066118ecbc1e1
Blessed_0712 <47700074+vusaeng@users.noreply.github.com>
2024-02-08 22:42:48 +0200

    New Robofriends app.

---
+++ src/Containers/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import CardList from "../Components/CardList";
+import SearchBox from '../Components/SearchBox';
+import './App.css';
+import Scroll from '../Components/Scroll';
+import ErrorBoundary from "../Components/ErrorBoundary";
+
+
+
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                this.setState({ robots: users })
+            });
+
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
+    }
+
+
+    render() {
+        /* 
+                const filteredRobots = this.state.robots.filter(robot => {
+                    return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+                })
+         */
+        // Or we could do DESTRUCTURING like this **************************************************
+
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+
+        if (robots.length === 0) {
+            return <h1 style={{ color: "blue" }}>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundary>
+
+                    </Scroll>
+
+                </div>
+            );
+        }
+    }
+}
+
+export default App;
